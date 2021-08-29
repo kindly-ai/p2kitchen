aeea5e9584753bfb8c683346800e52dab2226330
Nikolai R Kristiansen <nikolaik@gmail.com>
2021-08-29 21:20:21 +0200

    ✨ Add mockup dashboard

---
+++ frontend/vite.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
+import reactSvgPlugin from "vite-plugin-react-svg";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh(), reactSvgPlugin()],
+});
