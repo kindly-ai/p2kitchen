2334702b871ad4edd3024e3f7bc2b4784d24da23
Nikolai Kristiansen <nikolaik@gmail.com>
2021-09-09 10:37:25 +0200

    🐛 Fix prod frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
 import reactSvgPlugin from "vite-plugin-react-svg";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), reactSvgPlugin()],
+  define: {
+    __DEV__: process.env.NODE_ENV !== 'prodution'
+  }
 });
