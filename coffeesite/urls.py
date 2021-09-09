7c3cfff0719e0d11bdd8cb791ebb673677d35c49
Nikolai Kristiansen <nikolaik@gmail.com>
2021-09-09 11:35:25 +0200

    💩 Serve static files

--- coffeesite/urls.py
+++ coffeesite/urls.py
@@ -1,6 +1,13 @@
-from django.urls import include, path
+from django.conf import settings
 from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from p2coffee import urls as p2coffee_urls
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include(p2coffee_urls))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(p2coffee_urls)),
+    # FIXME: Hack in static file serving
+    re_path(r"^static/(?P<path>.*)$", serve, kwargs={"document_root": settings.STATIC_ROOT}),
+]
