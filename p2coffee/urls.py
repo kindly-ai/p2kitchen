7115ce51e08ec26f351bd9d346effced217254df
Nikolai R Kristiansen <nikolaik@gmail.com>
2021-09-26 21:18:41 +0200

    🔥 Remove sensor stats view

--- p2coffee/urls.py
+++ p2coffee/urls.py
@@ -3,14 +3,11 @@ from django.urls import path
 from p2coffee.views.kindly import KindlyOutgoingView
 from p2coffee.views.sensors import CreateSensorEventView
 from p2coffee.views.slack import SlackCommandView, SlackInteractionsView, SlackEventsView
-from p2coffee.views.stats import StatsView, StatsEvents
 from strawberry.django.views import AsyncGraphQLView
 from p2coffee.schema import schema
 
 urlpatterns = [
     path("event/log/", CreateSensorEventView.as_view(), name="create-log-event"),
-    path("stats/", StatsView.as_view(), name="stats"),
-    path("stats/events/", StatsEvents.as_view(), name="stats-events"),
     path("kindly/", KindlyOutgoingView.as_view(), name="kindly-outgoing"),
     path("slack/commands/", SlackCommandView.as_view(), name="slack-commands"),
     path("slack/interactions/", SlackInteractionsView.as_view(), name="slack-interactions"),
