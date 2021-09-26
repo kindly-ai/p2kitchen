e525b36fe15276bf17be4bf7bb843598f9a92945
Nikolai R Kristiansen <nikolaik@gmail.com>
2021-09-26 22:21:37 +0200

    🚨 Format js

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URI = import.meta.env.VITE_API_URI || 'http://localhost:8000';
+const API_URI = import.meta.env.VITE_API_URI || "http://localhost:8000";
 
 const client = new ApolloClient({
   uri: `${API_URI}/graphql`,
