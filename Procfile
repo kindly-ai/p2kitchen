cbf3e8da824845c143f484796e3564f255246691
Nikolai R Kristiansen <nrk@kindly.ai>
2022-08-29 14:45:02 +0200

    🔥 Remove django-rename-app

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 web: gunicorn p2kitchen.asgi:application -k uvicorn.workers.UvicornWorker
 worker: python manage.py run_huey -w 2
-# FIXME: Remove after deploy
-release: python manage.py rename_app p2coffee p2kitchen && python manage.py migrate --noinput
+release: python manage.py migrate --noinput
--- p2kitchen/settings.py
+++ p2kitchen/settings.py
@@ -11,20 +11,10 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "changeme")
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", True)
-
 TESTING = any(m in sys.modules for m in ["pytest", "py.test"])
-
 ALLOWED_HOSTS = ["*"] if DEBUG else os.getenv("ALLOWED_HOSTS", "").split(",")
-
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -40,14 +30,11 @@ INSTALLED_APPS = [
     "strawberry.django",
     "corsheaders",
     "channels",
-    "daphne",
-    "django_rename_app",
+    "daphne"
 ]
-
 INSTALLED_APPS += [
     "p2kitchen",
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
@@ -58,9 +45,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 ROOT_URLCONF = "p2kitchen.urls"
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -76,12 +61,9 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = "p2kitchen.wsgi.application"
 ASGI_APPLICATION = "p2kitchen.asgi.application"
-
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -90,19 +72,15 @@ DATABASES = {
 }
 if os.getenv("DATABASE_URL"):
     DATABASES["default"] = dj_database_url.config()
-
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 # Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
-
-
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
 LANGUAGES = [
     ("en", "English"),
 ]
@@ -112,7 +90,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = os.getenv("STATIC_URL", "/static/")
@@ -120,9 +97,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
-
+# CORS
 CORS_ALLOWED_ORIGINS = [
     "https://kitchen.kindly.ai",
     "https://kindly-kitchen.netlify.app",
@@ -131,11 +106,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",
     "http://localhost:8000",
 ]
-
-# Huey worker
+# Huey
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379")
 HUEY = RedisHuey("p2kitchen", results=False, immediate=TESTING, url=REDIS_URL)
-
+# Channels
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -144,13 +118,10 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
 # Slack
 SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")
 SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
 SLACK_CHANNEL = os.getenv("SLACK_CHANNEL", "#kitchen-dev")
-
-
 # Brewing settings
 BREWTIME_AVG_SECONDS = int(os.getenv("BREWTIME_AVG_SECONDS", "290"))  # 4 min and 50 seconds
 
